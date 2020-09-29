923d644ff78e25c479f64de555bd4754a1159abc
uriel zacarias <urielzacarias@gmail.com>
2020-09-29 00:01:40 -0700

    added onChange to form using useState constants

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({title: '', amount: ''});
+  const [ enteredTitle, setEnteredTitle] = useState();
+  const [ enteredAmount, setEnteredAmount] = useState();
 
 
   const submitHandler = event => {
@@ -19,27 +20,19 @@ const IngredientForm = React.memo(props => {
           <div className="form-control">
             <label htmlFor="title">Name</label>
             <input type="text" id="title" 
-              value={inputState[0].title}
+              value={enteredTitle}
               onChange={ event => {
-                const newTitle = event.target.value; 
-                inputState[1](prevInputState => ({
-                  title: newTitle,
-                  amount: prevInputState.amount
-                }))}
-              }
+                setEnteredTitle(event.target.value)
+              }}
             />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             <input type="number" id="amount" 
-              value={inputState[0].amount}
+              value={enteredAmount}
               onChange={ event => {
-                const newAmount = event.target.value; 
-                inputState[1](prevInputState => ({
-                  title: prevInputState.title,
-                  amount: newAmount
-                }))}
-              }
+                setEnteredAmount(event.target.value)
+              }}
             />
           </div>
           <div className="ingredient-form__actions">
